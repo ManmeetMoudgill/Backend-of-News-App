1018c4adcda6eac80ec2d94f46c0f1765f3fa263
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2022-04-10 22:50:37 +0200

    Files Added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 var cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 require('dotenv').config();
 
 //used the express.json() to parse the body of the request
